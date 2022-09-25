b9530bc71b17de68a5340102a352795f8d32f0a5
iamwill123 <williampeiyuan@gmail.com>
2022-09-25 07:44:43 -0400

    fix ts error

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,7 @@
-import { map, uniq, isNil, reject, pipe } from 'ramda'
+import { isNil, map, pipe, reject, uniq } from 'ramda'
 
 const isInt = (n: number) => {
   return n % 1 === 0
 }
 
-const clean = map(
-  pipe(reject(isNil), uniq),
-);
-export { isInt, clean }
+export { isInt }
